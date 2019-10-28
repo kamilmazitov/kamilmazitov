4b125419bd53dd54f4b883bbd93ffd0e33e7f63d
mazitov479 <mazitov.kamil.00@mail.ru>
2019-10-28 09:30:43 +0300

    controller_changes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,26 +1,25 @@
 class StoriesController < ApplicationController
 	
-	def new
-		@story = Story.new
-	end
+  def new
+    @story = Story.new
+  end
 
-	def index
-    	@stories = Story.all
-  	end
+  def index
+    @stories = Story.all
+  end
 
-	def create
-  		@story = Story.new(story_params)
+  def create
+    @story = Story.new(story_params)
+  	@story.save
+ 	redirect_to @story
+  end
 
-  		@story.save
- 		redirect_to @story
-	end
+  def show
+    @story = Story.find(params[:id])
+  end
 
-	def show
-    	@story = Story.find(params[:id])
-  	end
-
-	private
-	  	def story_params
-	    	params.require(:story).permit(:title, :content)
-	  	end
+  private
+    def story_params
+	  params.require(:story).permit(:title, :content)
+    end
 end
